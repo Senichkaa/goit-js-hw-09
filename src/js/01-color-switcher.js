b67c5aa55d1c2fe5593ffdacdc5a1713dec3240d
Senichkaa <super-kapanaiko406@ukr.net>
2023-07-07 14:26:25 +0300

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId;
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+stopButton.setAttribute('disabled', '');
+
+function onStartButtonClick() {
+  startButton.setAttribute('disabled', '');
+  stopButton.removeAttribute('disabled');
+  intervalId = setInterval(startColorChanging, 1000);
+}
+
+function startColorChanging() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  return startColorChanging;
+}
+
+function onStopButtonClick() {
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', '');
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
